47807a9512deea787a191c70556ed0c69f6db20a
Joel Känsälä <joel.kansala@innofactor.com>
2021-08-03 12:57:29 +0300

    day4 part 1 done

--- day4.py
+++ day4.py
@@ -4,23 +4,37 @@ from os import replace
 from libs.common import readPuzzleInputAsString
 
 def day4(puzzleInput):
+    valid_passports = 0
     expectedFields = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"}
+
+    # split passports from eachother
     passports = list(puzzleInput.split("\n\n"))
+    # remove newlines from passport strings
     passports = list(map(lambda x: x.replace("\n", " "), passports))
 
     for passport in passports:
         fields = passport.split()
-        id_fields = []
-        val_fields = []
+        id_fields = set()
+        val_fields = list()
         
         for field in fields:
             id, val = field.split(":")
-            id_fields.append(id)
+            id_fields.add(id)
             val_fields.append(val)
+
         passport_dict = dict(zip(id_fields, val_fields))
 
+        # find differing values in sets
         print(passport_dict)
+        missingFields = expectedFields.difference(id_fields)
+        if (len(missingFields) == 0):
+            valid_passports += 1
+        elif (len(missingFields) == 1 and missingFields.pop() == "cid"):
+            valid_passports += 1
+    
+    return valid_passports
 
 if __name__ == "__main__":
-    day4(readPuzzleInputAsString("puzzleInput/day4_test.txt"))
+    part1 = day4(readPuzzleInputAsString("puzzleInput/day4.txt"))
+    print(part1)
     pass
