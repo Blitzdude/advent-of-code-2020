9005929acef1c94833c02c7b5b9e6341fc6627d8
Joel Känsälä <joel.kansala@innofactor.com>
2021-08-05 10:13:20 +0300

    day5 part 1 works

--- day5.py
+++ day5.py
@@ -4,6 +4,13 @@ from libs.common import readPuzzleInput
 
 def day4(puzzleInput):
     print(puzzleInput)
+    seat_codes = list()
+    for line in puzzleInput:
+        seat_codes.append(decodeSeatCode(line))
+
+    sorted_seats = sorted(seat_codes)
+    highest_seat_code = sorted_seats.pop()
+    print(highest_seat_code)
 
 def decodeSeatCode(code):
     if (type(code) != str):
@@ -14,7 +21,21 @@ def decodeSeatCode(code):
         return None
 
     # plane has rows 0 - 127 and columns 0 - 7
-    
+    row_min, row_max = 0, 127
+    col_min, col_max = 0, 7
+    for c in code:
+        if (c == "B"):
+            row_min += int((row_max - row_min) / 2) + 1
+        elif (c == "F"):
+            row_max -= int((row_max - row_min) / 2) + 1
+        elif (c == "R"):
+            col_min += int((col_max - col_min) / 2) + 1
+        elif (c == "L"):
+            col_max -= int((col_max - col_min) / 2) + 1
+        
+    seat_code = row_min * 8 + col_min
+    return seat_code
+
 
     
 
